2997686bf92727f6ad1d93e7405d9d05c6dc7551
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-02-12 10:28:48 -0600

    added comments to js files

--- js/for_loops.js
+++ js/for_loops.js
@@ -2,7 +2,7 @@
 
 console.log("Hello, from loops js");
 
-// 1. Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+// TODO: 1. Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
 
 // For example, showMultiplicationTable(7) should output
 
@@ -32,7 +32,7 @@ console.log("Hello, from loops js");
 
 
 
-// 2. Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
+// TODO: 2. Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
 
 // 123 is odd
 // 80 is even
@@ -56,7 +56,7 @@ console.log("Hello, from loops js");
 
 
 
-// 3. Create a for loop that uses console.log to create the output shown below.
+// TODO: 3. Create a for loop that uses console.log to create the output shown below.
 
 // 1
 // 22
@@ -82,7 +82,7 @@ console.log("Hello, from loops js");
 // }
 
 
-// 4. Create a for loop that uses console.log to create the output shown below.
+// TODO: 4. Create a for loop that uses console.log to create the output shown below.
 
 // 100
 // 95
--- js/while.js
+++ js/while.js
@@ -3,7 +3,7 @@
 console.log("Hello, from loops js");
 
 
-// 1. Create a while loop that uses console.log() to create the output shown below.
+// TODO: 1. Create a while loop that uses console.log() to create the output shown below.
 
 // 2
 // 4
@@ -31,7 +31,7 @@ console.log("Hello, from loops js");
 // }
 
 
-// 2. An ice cream seller can't go home until she sells all of her cones. Write a JS program that generates a random number between 50 and 100 representing the amount of cones to sell. Your code should generate numbers between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is how you get the random numbers:
+// TODO: 2. An ice cream seller can't go home until she sells all of her cones. Write a JS program that generates a random number between 50 and 100 representing the amount of cones to sell. Your code should generate numbers between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is how you get the random numbers:
 
 // This is how you get a random number between 50 and 100
 // var allCones = Math.floor(Math.random() * 50) + 50;
