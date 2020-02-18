c581509ef0eac7f908520387f5e3b9d031ead788
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-02-18 10:37:39 -0600

    Files completed

--- js/circle.js
+++ js/circle.js
@@ -3,22 +3,29 @@
 
     // create a circle object
     var circle = {
-        radius: 3,
+        radius: 3, //object property
 
-        getArea: function () {
+        getArea: function (radius) {  //object method creation
             // TODO: complete this method
             // hint: area = pi * radius^2
+            var area = radius * radius * Math.PI ;
 
-            return; // TODO: return the proper value
+            return area; // TODO: return the proper value
         },
 
-        logInfo: function (doRounding) {
-            // TODO: complete this method.
+        logInfo: function (doRounding) { //object method creation
 
+            // TODO: complete this method.
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            if (doRounding === true){
+                Math.round(doRounding);
+            } else{
+                return doRounding;
+            }
+
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea(this.radius));
         }
     };
 
@@ -29,7 +36,9 @@
     circle.logInfo(true);
 
     console.log("=======================================================");
+
     // TODO: Change the radius of the circle to 5.
+    circle.radius = 5;
 
     // log info about the circle
     console.log("Raw circle information");
